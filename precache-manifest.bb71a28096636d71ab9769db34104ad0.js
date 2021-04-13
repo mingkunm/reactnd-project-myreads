473e7d3f00d2a6706c89db82f538a18098b6caee
mingkunma <mmk.ee.911@gmail.com>
2021-04-13 01:19:30 -0700

    Updates

---
+++ precache-manifest.bb71a28096636d71ab9769db34104ad0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "7b5626f29bcd002e58ea",
+    "url": "/reactnd-project-myreads-starter/static/css/main.995a74f2.chunk.css"
+  },
+  {
+    "revision": "7b5626f29bcd002e58ea",
+    "url": "/reactnd-project-myreads-starter/static/js/main.7b5626f2.chunk.js"
+  },
+  {
+    "revision": "740c84f90314358fd7cd",
+    "url": "/reactnd-project-myreads-starter/static/js/1.740c84f9.chunk.js"
+  },
+  {
+    "revision": "83f7d75ca12013b90cc6",
+    "url": "/reactnd-project-myreads-starter/static/js/runtime~main.83f7d75c.js"
+  },
+  {
+    "revision": "3687aa988bec28f17ae39153db1646c3",
+    "url": "/reactnd-project-myreads-starter/static/media/arrow-back.3687aa98.svg"
+  },
+  {
+    "revision": "bb9363b86b02777d23523851826414e0",
+    "url": "/reactnd-project-myreads-starter/static/media/add.bb9363b8.svg"
+  },
+  {
+    "revision": "78c6f53b2342643639fe1cbaf171bf47",
+    "url": "/reactnd-project-myreads-starter/static/media/arrow-drop-down.78c6f53b.svg"
+  },
+  {
+    "revision": "186cd5fbcb1b8eee1958c737576e951a",
+    "url": "/reactnd-project-myreads-starter/index.html"
+  }
+];
